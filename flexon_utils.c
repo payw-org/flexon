f88ff9ef0dc8e0d6cf1760506f52e44fcf29baf7
ihooni <to@ihooni.com>
2019-11-15 02:39:31 +0900

    Define the function `newType`

--- flexon_utils.c
+++ flexon_utils.c
@@ -9,3 +9,12 @@ void yyerror(const char *s) {
   fprintf(stderr, "Error: %s\n", s);
   exit(1);
 }
+
+UniversalType* newType(char *type, int size) {
+  UniversalType *new = (UniversalType*)malloc(sizeof(UniversalType));
+  new->type = type;
+  new->size = size;
+
+  return new;
+}
+
