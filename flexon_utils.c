6f5c92d10d00ce95ce0dea78641a291fd9d494ff
ihooni <to@ihooni.com>
2019-11-22 02:51:27 +0900

    chore: update print format

--- flexon_utils.c
+++ flexon_utils.c
@@ -19,7 +19,7 @@ void yaccError(int lineno, char *s, ...) {
   va_list ap;
   va_start(ap, s);
 
-  fprintf(stderr, "Line(%d) Error: ", lineno);
+  fprintf(stderr, "Line[%d] Error: ", lineno);
   vfprintf(stderr, s, ap);
   fprintf(stderr, "\n");
 }
